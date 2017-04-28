7e0112bb884f356a6e84941142cca2d17c0ccc39
Yury Shapkin <yshapkin@hotmail.com>
2017-04-29 00:52:06 +0300

    skeleton has been added

---
+++ src/app/main/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//import '../../../assets/stylesheets/styles.css';
+
+@Component({
+    selector: 'main-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+}
